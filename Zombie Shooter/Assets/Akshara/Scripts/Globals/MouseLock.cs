f82996f635325387bee62ed0c9ccfc6655b121d5
Aksharamurda <aksharamurda@gmail.com>
2018-10-02 00:35:12 +0700

    Camera Prefabs Added

---
+++ Zombie Shooter/Assets/Akshara/Scripts/Globals/MouseLock.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace AksharaMurda
+{
+    public class MouseLock : MonoBehaviour
+    {
+        private bool _isLocked = true;
+
+        private void LateUpdate()
+        {
+            if (SettingManager.instance.useMobileConsole)
+                return;
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+                _isLocked = false;
+
+            if (Input.GetMouseButtonDown(0))
+                _isLocked = true;
+
+            if (_isLocked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
